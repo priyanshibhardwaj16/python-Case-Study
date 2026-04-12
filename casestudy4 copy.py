c2c737ec2ba17b6f433edcba0a1dd3868568c6c6
priyanshibhardwaj16 <priyanshibhardwaj714@gmail.com>
2026-04-12 22:33:16 +0530

    new case study

---
+++ casestudy4 copy.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+
+exam_data = {
+    'name': ['Anastasia','Dima','Katherine','James','Emily'],
+    'score': [12.5, 9, 16.5, np.nan, 9],
+    'attempts': [1, 3, 2, 3, 2],
+    'qualify': ['yes','no','yes','no','no']
+}
+
+df = pd.DataFrame(exam_data)
+
+print(df)  
+
+#labels = ['a','b','c','d','e']
+#df = pd.DataFrame(exam_data,index=labels)
+
+#df[df['score'].isnull()] 
+
+#df[df['attempts'] > 2]  
+
+df[(df['attempts'] < 2) & (df['score'] > 15)]
